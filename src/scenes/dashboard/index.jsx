cd3b0521e2289d57a0e68cab041894d8fae006c4
RaW <akram.iiuc.ctg@gmail.com>
2023-11-29 21:46:24 +0600

    Sidebar and component added

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,10 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
-const dashboard = () => {
+const Dashboard = () => {
     return (
-        <div>
-            
-        </div>
+        <Box className="m-[20px]">
+            <Header title="Dashboard" subtitle="Welcome to your Dashboard"/>
+        </Box>
     );
 };
 
-export default dashboard;
+export default Dashboard;
